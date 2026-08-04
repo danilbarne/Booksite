2231bedddcb2cacb6c6e2d83ccabd23ab9767ebf
danilbarne <danilbaranets064@gmail.com>
2026-08-04 21:51:25 +0300

    Исправил config: теперь берёт настройки почты из Environment

--- config.py
+++ config.py
@@ -23,10 +23,11 @@ class Config:
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    MAIL_SERVER = "smtp.gmail.com"
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-
+    # === ИЗМЕНЕНИЕ: БЕРЁМ ВСЁ ИЗ ПЕРЕМЕННЫХ ОКРУЖЕНИЯ ===
+    MAIL_SERVER = os.getenv("MAIL_SERVER", "smtp.gmail.com")  # По умолчанию gmail, если ничего нет
+    MAIL_PORT = int(os.getenv("MAIL_PORT", 587))
+    MAIL_USE_TLS = os.getenv("MAIL_USE_TLS", "True").lower() == "true"
+    
     MAIL_USERNAME = os.getenv("MAIL_USERNAME")
     MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
     MAIL_DEFAULT_SENDER = os.getenv("MAIL_USERNAME")
